eb635ff1123dc699159b73c5d405ff9f88196245
Allan <amramogo@gmail.com>
2024-03-06 00:12:53 +0300

    Doing schema and backend authentication using convex

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,13 @@
 import Image from "next/image";
 import { Button } from "@/components/ui/button"
 import { SignInButton, SignedIn, SignedOut, SignOutButton } from "@clerk/nextjs";
-
+import { useMutation, useQuery } from "convex/react";
+import { api } from "../../convex/_generated/api";
 
 
 export default function Home() {
+  const createFile = useMutation(api.files.createFile);
+  const files = useQuery(api.files.getFiles);
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <SignedIn>
@@ -20,8 +23,19 @@ export default function Home() {
         </SignInButton>
       </SignedOut>
 
+      {files?.map(file => {
+        return <div key={file._id}>{file.name}</div>
+      })}
 
-      <Button>Testing 123</Button>
+      <Button
+        onClick={() => {
+          createFile({
+            name: "Hello World"
+          });
+        }}
+      > 
+        Upload file
+      </Button>
        
       
     
